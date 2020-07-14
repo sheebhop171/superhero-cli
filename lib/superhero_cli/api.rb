2d53c40d0b6f8f896cdf140e49835ec6055ed411
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-14 16:21:38 +0000

    refactoring api.rb code

--- lib/superhero_cli/api.rb
+++ lib/superhero_cli/api.rb
@@ -4,20 +4,13 @@ class SuperheroCLI::API
 
   def initialize(input)
     @input = input
-   # @key = key #input API key here
   end
 
   def get_superheroes
-  #   binding.pry
     url = "https://superheroapi.com/api/10218887433359734/search/#{input}"
     response = HTTParty.get(url)
     superhero = response["results"]
     superhero_name = superhero[0]["name"]
-    binding.pry
-    superhero.each_with_index { |index, name|
-      puts "#{index}: #{name}" }
   end
 
 end
-
-#response.map {|x| x.values[0]}
