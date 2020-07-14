b22fad38a4f76f97a8e371ea2870f81f178211fe
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-14 22:18:41 +0000

    further definning attributes

--- lib/superhero_cli/api.rb
+++ lib/superhero_cli/api.rb
@@ -9,10 +9,18 @@ class SuperheroCLI::API
   def get_superheroes
     url = "https://superheroapi.com/api/10218887433359734/search/#{input}"
     response = HTTParty.get(url)
+    binding.pry
     superhero = response["results"]
     @name = superhero[0]["name"]
-    puts @name
+    @powerstats = superhero[0]["powerstats"].each do |stat| 
+    puts "#{stat}" 
+      end
+    @full_name = superhero[0]["biography"]["full-name"]
+    @alter_egos = superhero[0]["biography"]["alter-egos"]
+    @place_of_birth = superhero[0]["biography"]["place-of-birth"]
+    @occupation = superhero[0]["work"]["occupation"]
     puts 
   end
 
 end
+  
