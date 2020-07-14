8fab2fe3d7d5b9d9c8a67c5db389be51202b829c
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-14 16:19:04 +0000

    update @superheroes_list method

--- lib/superhero_cli/cli.rb
+++ lib/superhero_cli/cli.rb
@@ -5,30 +5,32 @@ class SuperheroCLI::CLI
   def start
     puts "Welcome to the ultimate Superhero App!"
     while @input != "exit"
-    # list superheros to choose from 
+    
     superheroes_list 
-    ask_for_superhero
+    
+    binding.pry 
+    
+    get_user_input
     api = SuperheroCLI::API.new(input)
-    api.get_superheroes
+    
     #list superhero information 
+    
     end 
     goodbye
   end
   
    def superheroes_list
-    # code superhero options to display  
-    @superheroes = ["Aquaman", "Black Widow", "Captain America", "Deadpool", "Hulk", "Iron Man", "Storm", "Wonder Woman"]
-    binding.pry 
+    puts "Enter the name of the superhero you'd like to learn more about."
+    
+    @superheroes = ["Aquaman", "Deadpool", "Hulk", "Phoenix", "Storm"]
     @superheroes.each.with_index(1) do |name, index|
       puts "#{index}. #{name}"
     end
   end 
 
-  def ask_for_superhero
-    puts "Enter the name of the superhero you'd like to learn more about."
-    @input = gets.strip 
+  def get_user_input
+   @input = gets.strip 
   end
-  
   
   def goodbye
    puts "Thank you for visiting the Superhero App!"
