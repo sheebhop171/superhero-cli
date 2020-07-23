8ea07f846b8fe3498733526bcae48e6a9768772e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-23 23:30:26 +0000

    Cleaning up terminal formatting

--- lib/superhero_cli/cli.rb
+++ lib/superhero_cli/cli.rb
@@ -7,13 +7,13 @@ class SuperheroCLI::CLI
     intro
     list_superheroes
     choose_superhero
-
+    menu
   end
 
   def intro
     puts ""
     puts "--------------------------------------"
-    puts "Welcome to the ultimate Superhero App!".colorize(:yellow)
+    puts "Welcome to the ultimate Superhero App!".colorize(:blue)
     puts "--------------------------------------"
     puts ""
     puts "You've been recruited for a secret mission to take down an evil villian.".colorize(:yellow)
@@ -23,7 +23,7 @@ class SuperheroCLI::CLI
 
   def list_superheroes
     puts ""
-    puts "Below is your list of viable candidates:"
+    puts "Below is your list of viable candidates:".colorize(:yellow)
     puts ""
     superheroes = ["Aquaman", "Batgirl", "Batman", "Deadpool", "Firestorm", "Hulk", "Maverick", "Mystique", "Phoenix", "Spider-Man"]
     superheroes.each.with_index(1) do |name, index|
@@ -33,8 +33,8 @@ class SuperheroCLI::CLI
 
   def choose_superhero
     puts ""
-    puts "Enter the name of the superhero you'd like to learn more about."
-    puts "---------------------------------------------------------------"
+    puts "Enter the name of the superhero you'd like to learn more about:".colorize(:yellow)
+    puts ""
     input = gets.strip
     SuperheroCLI::API.get_superhero(input)
     display_superhero_data(input)
@@ -42,7 +42,11 @@ class SuperheroCLI::CLI
 
   def display_superhero_data(input)
     input = SuperheroCLI::Superhero.all.find {|superhero| superhero.input == input}
-    puts "-----------Superhero Info-----------"
+    puts ""
+    puts "------------------".colorize(:blue)
+    puts "Superhero Profile".colorize(:blue)
+    puts "------------------".colorize(:blue)
+    puts ""
     puts "Name: #{input.name}"
     puts "Full Name: #{input.full_name}"
     puts "Alter Egos: #{input.alter_egos}"
@@ -52,20 +56,25 @@ class SuperheroCLI::CLI
     input.powerstats.each do |key, value|
        puts " - #{key}: #{value}"
     end
+    puts ""
   end
 
   def menu
+    puts "Would you like to see another superhero?".colorize(:yellow)
+    puts "Type 'yes' to see the list of superheroes or 'no' to exit.".colorize(:yellow)
     input = ""
     while input
       input = gets.strip.downcase
       case input
       when "yes"
         list_superheroes
+        choose_superhero
+        menu
       when "no"
         exit
         break
       else
-        puts "I don't understand that. You can enter 'yes' or 'no'."
+        puts "I don't understand that. You can enter 'yes' or 'no'.".colorize(:red)
       end
     end
   end
